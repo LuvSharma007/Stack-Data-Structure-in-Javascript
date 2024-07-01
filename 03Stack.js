c069b551984b4f7de835c583cb7f599b5bf429e2
Luv Sharma <141054252+LuvSharma007@users.noreply.github.com>
2024-07-01 13:57:25 +0530

    Add files via upload

---
+++ 03Stack.js
@@ -0,0 +1,75 @@
+// Stack Data Structure
+
+// Stack is a linear data structure that follows a particular order in which the operations are performed. The order may be LIFO(Last In First Out) or FILO(First In Last Out). LIFO implies that the element that is inserted last, comes out first and FILO implies that the element that is inserted first, comes out last.
+
+
+
+// let data = [];
+// let currentSize = data.length;
+// let max = 5;
+
+// // this function will insert the element at the top of the stack
+
+
+// function push(value){
+//     if(currentSize>=max){
+//         alert("Stack is Full !");
+//          return;
+//     }
+//     data[currentSize] = value;
+//     currentSize++;
+   
+// }
+
+// // this Funciton will remove the 
+
+// function pop(){
+//     if(currentSize>0){
+//         currentSize=currentSize-1;
+//         data.length=currentSize;
+//     }else{
+//         alert("stack is already Empty !");
+//     }
+// }
+
+// push(20);
+// push(40);
+// push(60);
+// push(80);
+// push(100);
+
+// pop();
+
+// push(120);
+// console.log(data);
+
+
+// reverse a string in stack
+
+// let data = [];
+// let currentSize = data.length;
+
+// function push(value){
+//     data[currentSize] = value;
+//     currentSize++;
+// }
+
+// function pop(){
+//     data[currentSize] = currentSize -1;
+//     data.length = currentSize;
+// }
+
+// function reverse(value){
+//     for(let i=0; i<value.length; i++){
+//         push(value[i]);
+//     }
+// }
+
+// let str = "Anil";
+// str = str.split("");
+// reverse(str);
+// // console.log(str);
+// console.log(data);
+
+//in complete
+
